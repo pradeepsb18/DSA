29795dcf3e0d6457a044891b3810696db4df9b69
pradeepsb18 <pradeepshankaragoud.cs24@bmsce.ac.in>
2025-12-08 11:41:55 +0530

    Add files via upload

---
+++ bfs.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 20
+
+int queue[MAX], front = -1, rear = -1;
+int visited[MAX];
+
+void enqueue(int v) {
+    if (rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (front == -1)
+        front = 0;
+    queue[++rear] = v;
+}
+
+int dequeue() {
+    if (front == -1 || front > rear)
+        return -1;
+    return queue[front++];
+}
+
+void BFS(int adj[MAX][MAX], int n, int start) {
+    int i, current;
+
+    for (i = 0; i < n; i++)
+        visited[i] = 0;
+
+    enqueue(start);
+    visited[start] = 1;
+
+    printf("BFS Traversal: ");
+
+    while ((current = dequeue()) != -1) {
+        printf("%d ", current);
+
+        for (i = 0; i < n; i++) {
+            if (adj[current][i] == 1 && !visited[i]) {
+                enqueue(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+int main() {
+    int adj[MAX][MAX];
+    int n, i, j, start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    BFS(adj, n, start);
+
+    return 0;
+}
